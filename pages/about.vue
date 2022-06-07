74684a6bbae7d90a8cf0642cc0b183f1454eabe8
Muhammad Areeb <mareeb@sunbonn.com>
2022-06-07 16:33:28 +0500

    project has been setup successfully.

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about-page">
+      <h1>About Hello MJJ</h1>
+      <p>This is an app</p>
+  </div>
+
+</template>
+ <script>
+export default {
+    head(){
+        return {
+            title: "About Us",
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'Description',
+                    content: 'This is an app'
+                }           
+            ]
+        }
+    }
+
+}; 
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: "WELCOME TO MJJ",
+        meta: [{
+          hid: 'description',
+          name: 'Description',
+          content: 'This is an app'
+        }]
+      }
+    }
+
+  }
+
+</script>
